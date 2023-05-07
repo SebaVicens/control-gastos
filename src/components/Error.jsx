1faea72bd4784cb5869559b5c75f67daccc39d81
SebaVicens <vicens279@gmail.com>
2023-05-07 15:28:55 -0300

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Error = ({}) => {
+    
+  return (
+    <div className="bg-red-800 text-white text-center py-2 my-3 uppercase rounded-lg">
+        <p>No es un presupuesto valido</p>
+    </div>
+  )
+}
+
+export default Error
